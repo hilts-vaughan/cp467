f9981d9221b3bf32f0377e739ab3e27f0a37eb9b
novon229 <novon229@gmail.com>
2015-10-09 15:03:50 -0400

    Thing
    Basic outline for filtering. Gets a window based on the tif image and
    applies a broken filter to it.

--- __main__.py
+++ __main__.py
@@ -2,3 +2,47 @@ __author__ = 'touma::setsuna'
 
 # Just a statement built-in to fill Git with something
 print("Initializing assignment... developers: ", ", ".join(['Vaughan Hilts', 'Brandon Smith'][::-1]))
+
+from PIL import Image
+import math
+def sum_of_filters(pixels, filter):
+    value=0
+    for x in range(len(pixels[0])):
+        for y in range(len(filter[0])):
+            value+=value+pixels[x][y]*filter[x][y]
+    return value
+
+im = Image.open('a_image.tif')
+pix_array=im.load()
+size=im.size
+high_pass_filter = [[-1, -1, -1], [-1,  8, -1],[-1, -1, -1]]
+
+fill=Image.new(im.mode,im.size,0)
+print(size[0])#width so row
+
+for k in range(size[0]-1):
+        for i in range(size[1]-1):
+            if(k==0):#first row
+                if(i==0):
+                    pixels=[[0,0,0],[0,pix_array[k,i],pix_array[k,i+1]],[0,pix_array[k+1,i],pix_array[k+1,i+1]]]
+                elif(i==size[1]):
+                    pixels=[[0,0,0],[pix_array[k,i-1],pix_array[k,i],0],[pix_array[k+1,i+1],pix_array[k+1,i],0]]
+                else:
+                    pixels=[[0,0,0],[pix_array[k,i-1],pix_array[k,i],pix_array[k,i+1]],[pix_array[k+1,i-1],pix_array[k+1,i],pix_array[k+1,i+1]]]
+            elif(k==size[0]):
+                if(i==0):
+                    pixels=[[0,pix_array[k-1,i],pix_array[k-1,i+1]],[0,pix_array[k,i],pix_array[k,i+1]],[0,0,0]]
+                elif(i==size[1]):
+                    pixels=[[pix_array[k-1,i-1],pix_array[k-1,i],0],[pix_array[k,i-1],pix_array[k,i],0],[0,0,0]]
+                else:
+                    pixels=[[pix_array[k-1,i-1],pix_array[k-1,i],pix_array[k-1,i+1]],[pix_array[k,i-1],pix_array[k,i],pix_array[k,i+1]],[0,0,0]]
+
+            elif(i==0):
+                 pixels=[[0,pix_array[k-1,i],pix_array[k-1,i+1]],[0,pix_array[k,i],pix_array[k,i+1]],[0,pix_array[k+1,i],pix_array[k+1,i+1]]]
+            elif(i==size[1]):
+                pixels=[[pix_array[k-1,i-1],pix_array[k-1,i],0],[pix_array[k,i-1],pix_array[k,i],0],[pix_array[k+1,i-1],pix_array[k+1,i],0]]
+            else:
+                pixels=[[pix_array[k-1,i-1],pix_array[k-1,i],pix_array[k-1,i+1]],[pix_array[k,i-1],pix_array[k,i],pix_array[k,i+1]],[pix_array[k+1,i-1],pix_array[k+1,i],pix_array[k+1,i+1]]]
+            print(sum_of_filters(pixels,high_pass_filter))
+            fill.putpixel([k,i],sum_of_filters(pixels,high_pass_filter))
+fill.save("testimage.png")
