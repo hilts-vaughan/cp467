108a8e64e3f9b9c5dbc59ed3bdbcd8d02347335b
Vaughan Hilts <hilts.vaughan@gmail.com>
2015-10-09 00:28:24 -0400

    Initial commit with PyCharm project; still need to add some library modules such as SciPy

---
+++ __main__.py
@@ -0,0 +1,4 @@
+__author__ = 'touma::setsuna'
+
+# Just a statement built-in to fill Git with something
+print("Initializing assignment... developers: ", ", ".join(['Vaughan Hilts', 'Brandon Smith'][::-1]))
