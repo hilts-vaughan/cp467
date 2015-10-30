644a08c0f00e7193be5a30f95a5cdfe2f3e0ad59
novon229 <novon229@gmail.com>
2015-10-30 17:01:26 -0400

    filters running
    get filters running on images

--- __main__.py
+++ __main__.py
@@ -6,57 +6,42 @@ print("Initializing assignment... developers: ", ", ".join(['Vaughan Hilts', 'Br
 from PIL import Image
 
 import math
-def sum_of_filters(pixels, filter):
-    value=0
-    for x in range(len(pixels[0])):
-        for y in range(len(filter[0])):
-            value+=value+pixels[x][y]*filter[x][y]
-    return value
+def safe_pixel_getter(pixels,x,y):
 
-im = Image.open('a_image.tif')
+    if x<0 or y<0 or x > im.size[0] or y> im.size[1]:
+        return 0
+    return pixels[x,y]
+
+
+
+im = Image.open('download_again.jpg').convert('L')
 pix_array=im.load()
 size=im.size
-kernel = [[0, 0, 0], [0,  1, 0],[0, 0, 0]]
-
+kernel = [[-1, -1, -1,-1,-1], [-1, -1,  15, -1, -1],[-1, -1, -1, -1, -1]]
+print(kernel)
 fill=Image.new(im.mode,im.size,0)
 
 print(size[0])#width so row
-
+half_width=math.floor(len(kernel)/2)
+half_height=math.floor(len(kernel[0])/2)
+print(half_height)
+print(half_width)
 for k in range(size[0]-1):
         for i in range(size[1]-1):
+            acc = 0
+            for x in range(-half_width,half_width):
+                for y in range(-half_height,half_height):
+                    currentPix = safe_pixel_getter(pix_array, k + x, i + y)
+
+                    kernelPix = kernel[x + half_width][y + half_height]
+                    acc += currentPix*kernelPix
+            fill.putpixel((k,i),math.floor(acc))
+
+                    # pixels[len(kernel)-x,len(kernel[0])-y]=pix_array[safe_pixel_getter(pix_array, k-x, i-y)] #get the current pixel array. pix_array is not an array of pixels does not have a len function
 
             # Set accumlator
-            acc = 0
+            # pixels=[[pix_array[k-1,i-1],pix_array[k-1,i],pix_array[k-1,i+1]],[pix_array[k,i-1],pix_array[k,i],pix_array[k,i+1]],[pix_array[k+1,i-1],pix_array[k+1,i],pix_array[k+1,i+1]]]
 
-            try:
-                pixels=[
-                    [
-                        pix_array[k-1,i-1],pix_array[k-1,i],pix_array[k-1,i+1]],[pix_array[k,i-1],pix_array[k,i],pix_array[k,i+1]],[pix_array[k+1,i-1],pix_array[k+1,i],pix_array[k+1,i+1]]
-                ]
 
-            # if(k==0):#first row
-            #     if(i==0):
-            #         pixels=[[0,0,0],[0,pix_array[k,i],pix_array[k,i+1]],[0,pix_array[k+1,i],pix_array[k+1,i+1]]]
-            #     elif(i==size[1]):
-            #         pixels=[[0,0,0],[pix_array[k,i-1],pix_array[k,i],0],[pix_array[k+1,i+1],pix_array[k+1,i],0]]
-            #     else:
-            #         pixels=[[0,0,0],[pix_array[k,i-1],pix_array[k,i],pix_array[k,i+1]],[pix_array[k+1,i-1],pix_array[k+1,i],pix_array[k+1,i+1]]]
-            # elif(k==size[0]):
-            #     if(i==0):
-            #         pixels=[[0,pix_array[k-1,i],pix_array[k-1,i+1]],[0,pix_array[k,i],pix_array[k,i+1]],[0,0,0]]
-            #     elif(i==size[1]):
-            #         pixels=[[pix_array[k-1,i-1],pix_array[k-1,i],0],[pix_array[k,i-1],pix_array[k,i],0],[0,0,0]]
-            #     else:
-            #         pixels=[[pix_array[k-1,i-1],pix_array[k-1,i],pix_array[k-1,i+1]],[pix_array[k,i-1],pix_array[k,i],pix_array[k,i+1]],[0,0,0]]
-            #
-            # elif(i==0):
-            #      pixels=[[0,pix_array[k-1,i],pix_array[k-1,i+1]],[0,pix_array[k,i],pix_array[k,i+1]],[0,pix_array[k+1,i],pix_array[k+1,i+1]]]
-            # elif(i==size[1]):
-            #     pixels=[[pix_array[k-1,i-1],pix_array[k-1,i],0],[pix_array[k,i-1],pix_array[k,i],0],[pix_array[k+1,i-1],pix_array[k+1,i],0]]
-            # else:
-            #     pixels=[[pix_array[k-1,i-1],pix_array[k-1,i],pix_array[k-1,i+1]],[pix_array[k,i-1],pix_array[k,i],pix_array[k,i+1]],[pix_array[k+1,i-1],pix_array[k+1,i],pix_array[k+1,i+1]]]
 
-            print(sum_of_filters(pixels,kernel))
-            fill.putpixel([k,i],sum_of_filters(pixels,kernel))
-
-fill.save("testimage.tif")
+fill.save("testimage2.png")
