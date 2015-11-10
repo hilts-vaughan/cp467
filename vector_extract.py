7e0e1c7d2aef30399b8bdcf8a8d636860ff4d41b
Vaughan Hilts <vhilts@axonify.com>
2015-11-09 21:02:03 -0500

    Added basic vector extraction implementation

---
+++ vector_extract.py
@@ -0,0 +1,56 @@
+from math import *
+
+__author__ = 'vaughanhilts'
+
+
+# Extracts a feature vector from a given chunk mechanism.
+# Allows extracting from images, simply over-riding the
+# accumlator function to implement your logic for the vector
+# ratios
+class FeatureExtractor:
+
+    def __init__(self, image):
+        self.image = image
+
+    # Given an image and number of chunks in both axis, gives the
+    # vector back containins chunksWide * chunksHigh elements
+    def extract_vector(self, chunksWide, chunksHigh):
+        vector = []
+
+        for x in range(0, chunksWide):
+            for y in range(0, chunksHigh):
+                bounds = self._get_chunk_bound(chunksWide, chunksHigh, x, y)
+                # Compute vector for bounds
+                vector.append(self._get_value_for_bound(bounds))
+        return vector
+
+    def _get_chunk_bound(self, chunksWide, chunksHigh, x, y):
+        size = self.image.size
+        imageWidth = size[0]
+        imageHeight = size[1]
+
+        blockWidth =  imageWidth / chunksWide
+        blockHeight = imageHeight / chunksHigh
+
+        lowerX = blockWidth * x
+        lowerY = blockHeight * y
+
+        highX =  math.min(imageWidth, blockWidth * (x + 1))
+        highY =  math.min(imageHeight, blockHeight * (y + 1))
+
+        return [(lowerX, lowerY), (highX, highY)]  # Returns the empty bound
+
+    def _get_value_for_bound(self, bounds):
+        lower = bounds[0]
+        upper = bounds[1]
+        acc = 0
+        total = 0
+
+        # Get the vector value
+        for x in range(lower[0], upper[0]):
+            for y in range(lower[1], upper[1]):
+                if self.image[x, y] == 255:
+                    acc = acc + 1
+                total = total + 1
+
+        return acc / total
